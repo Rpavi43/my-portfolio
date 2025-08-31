73386e5d7de0ebd8c7d24265df6d9f564f9af562
Rasuri Pavithra <pavithrareddy2104@gmail.com>
2025-08-31 17:28:39 +0530

    Added GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my-portfolio/", // 👈 repo name (important for GitHub Pages)
   server: {
     open: true, // auto-open browser on npm run dev
   },
